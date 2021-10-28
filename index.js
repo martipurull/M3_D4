244c5144e680ac2f6816fefaa5e812dd0b6d6dd3
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-28 16:48:45 +0200

    done until first extra but not search functionality

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 window.onload = () => {
 
-    const addBooks = () => fetch('https://striveschool-api.herokuapp.com/books')
+    const loadBooks = () => fetch('https://striveschool-api.herokuapp.com/books')
         .then((response) => response.json())
         .then(receivedData => {
-            console.log(receivedData)
             receivedData.forEach(book => {
                 const bookCardRow = document.querySelector('.col-9 .row')
                 const bookCardCol = document.createElement('div')
@@ -44,7 +43,7 @@ window.onload = () => {
             })
         })
 
-    addBooks()
+    loadBooks()
 
     const addToCart = (event) => {
         const selectedCardTitle = event.target.parentNode.children[1].children[0]
@@ -52,6 +51,16 @@ window.onload = () => {
         const cartItem = document.createElement('li')
         cartItem.classList.add('list-group-item')
         cartItem.innerText = selectedCardTitle.innerText
+        const removeBtn = document.createElement('span')
+        removeBtn.classList.add('btn')
+        removeBtn.classList.add('btn-sm')
+        removeBtn.classList.add('btn-danger')
+        removeBtn.innerText = 'x'
+        removeBtn.addEventListener('click', function (e) {
+            e.target.parentNode.remove()
+            emptyBasketMessage.classList.toggle('d-none')
+        })
+        cartItem.appendChild(removeBtn)
         cartList.appendChild(cartItem)
         const emptyBasketMessage = document.querySelector('.list-group .text-dark>li')
         emptyBasketMessage.classList.add('d-none')
@@ -61,28 +70,76 @@ window.onload = () => {
         inCartBadge.classList.add('badge-warning')
         inCartBadge.innerText = 'already in your cart!'
         selectedCard.prepend(inCartBadge)
+        const deleteAllBtn = document.getElementById('deleteAllBtn')
+        deleteAllBtn.classList.remove('d-none')
+        deleteAllBtn.addEventListener('click', emptyCart)
     }
 
 
 
+    const filterBooks = () => fetch('https://striveschool-api.herokuapp.com/books')
+        .then((response) => response.json())
+        .then(receivedData => {
+            const searchInputValue = document.querySelector('form input').value
+            console.log(searchInputValue)
+            return receivedData.filter(book => book.title.includes(searchInputValue))
+        })
+        .then(filteredData => {
+            filteredData.forEach(book => {
+                const bookCardRow = document.querySelector('.col-9 .row')
+                const bookCardCol = document.createElement('div')
+                bookCardCol.classList.add('col-6')
+                bookCardCol.classList.add('col-md-4')
+                bookCardCol.classList.add('col-lg-3')
+                bookCardCol.classList.add('my-3')
+                bookCardRow.appendChild(bookCardCol)
+                const bookCard = document.createElement('div')
+                bookCard.classList.add('card')
+                bookCard.classList.add('h-100')
+                bookCard.classList.add('text-dark')
+                bookCard.innerHTML = `
+                    <img src="${ book.img }" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h6 class="card-text">${ book.title }</h6>
+                    <p class="card-text">£${ book.price }</p>
+                    <small class="card-text">${ book.category }</small>
+                    </div>
+                `
+                bookCardCol.appendChild(bookCard)
+                const addToCartBtn = document.createElement('button')
+                addToCartBtn.classList.add('btn')
+                addToCartBtn.classList.add('btn-success')
+                addToCartBtn.innerText = 'add to cart'
+                addToCartBtn.addEventListener('click', addToCart)
+                bookCard.appendChild(addToCartBtn)
+                const skipBtn = document.createElement('button')
+                skipBtn.classList.add('btn')
+                skipBtn.classList.add('btn-secondary')
+                skipBtn.classList.add('btn-sm')
+                skipBtn.classList.add('my-1')
+                skipBtn.innerText = 'hide book'
+                skipBtn.addEventListener('click', function () {
+                    bookCard.classList.add('d-none')
+                })
+                bookCard.appendChild(skipBtn)
+            })
+        })
 
 
+    const makeSearchBtn = () => {
+        const searchBtn = document.getElementById('searchBtn')
+        searchBtn.addEventListener('click', filterBooks)
+    }
+    makeSearchBtn()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const emptyCart = () => {
+        const cartList = document.querySelector('.list-group .text-dark')
+        cartList.innerHTML = ''
+        cartList.innerHTML = `<li class="list-group-item">Your basket is empty! <i class="bi bi-emoji-frown"></i>`
+        const deleteAllBtn = document.getElementById('deleteAllBtn')
+        deleteAllBtn.classList.add('d-none')
+        const addedToCartBadges = document.querySelectorAll('.badge-warning')
+        addedToCartBadges.forEach(badge => badge.remove())
+    }
 
 }
